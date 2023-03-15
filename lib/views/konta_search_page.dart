c0fa46d3c6b22a41110dbd59e6ae39ad06338eac
wkedsxx <dawid.debest@gmail.com>
2023-03-15 16:21:13 +0100

    Rozpoczęto dodawanie listy rozwijanej do filtrowania

--- lib/views/konta_search_page.dart
+++ lib/views/konta_search_page.dart
@@ -16,90 +16,129 @@ class _KontaSearchPageState extends State<KontaSearchPage> {
   @override
   Widget build(BuildContext context) {
     CollectionReference konta = FirebaseFirestore.instance.collection('konta');
+    CollectionReference hosty = FirebaseFirestore.instance.collection('hosty');
     String selectedRowDocId = '0';
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            ElevatedButton.icon(
-              onPressed: () async {
-                await FirebaseAuth.instance.signOut();
-                Navigator.of(context).push(
-                    MaterialPageRoute(builder: (context) => const LoginPage()));
-              },
-              icon: const Icon(Icons.logout),
-              label: const Text('Wyloguj'),
-            ),
-            ElevatedButton.icon(
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Center(
+          child: Column(
+            children: [
+              ElevatedButton.icon(
+                onPressed: () async {
+                  await FirebaseAuth.instance.signOut();
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const LoginPage()));
+                },
+                icon: const Icon(Icons.logout),
+                label: const Text('Wyloguj'),
+              ),
+              ElevatedButton.icon(
+                  onPressed: () => Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const KontrahenciPage())),
+                  icon: const Icon(Icons.groups),
+                  label: const Text('Kontrahenci')),
+              ElevatedButton.icon(
                 onPressed: () => Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => const KontrahenciPage())),
-                icon: const Icon(Icons.groups),
-                label: const Text('Kontrahenci')),
-            ElevatedButton.icon(
-              onPressed: () => Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => const KontoNewPage())),
-              icon: const Icon(Icons.person_add),
-              label: const Text('Dodaj konto'),
-            ),
-            FutureBuilder(
-                future: konta.get(),
-                builder: (BuildContext context,
-                    AsyncSnapshot<QuerySnapshot> snapshot) {
-                  if (snapshot.connectionState == ConnectionState.done) {
-                    List<dynamic> kontaList = snapshot.data!.docs.map((doc) {
-                      dynamic tempMap = doc.data();
-                      tempMap['id'] = doc.id;
-                      return tempMap;
-                    }).toList();
-                    return DataTable(showCheckboxColumn: false, columns: const <
-                        DataColumn>[
-                      DataColumn(
-                          label: Text('Login',
-                              style: TextStyle(fontWeight: FontWeight.bold))),
-                      DataColumn(
-                          label: Text('Kontrahent',
-                              style: TextStyle(fontWeight: FontWeight.bold))),
-                      DataColumn(
-                          label: Text('Host',
-                              style: TextStyle(fontWeight: FontWeight.bold))),
-                      DataColumn(
-                          label: Text('Typ konta',
-                              style: TextStyle(fontWeight: FontWeight.bold))),
-                      DataColumn(
-                          label: Text('Uwagi',
-                              style: TextStyle(fontWeight: FontWeight.bold))),
-                    ], rows: <DataRow>[
-                      for (var konto in kontaList)
-                        DataRow(
-                            onSelectChanged: (value) {
-                              if (selectedRowDocId == konto['id']) {
-                                ScaffoldMessenger.of(context)
-                                    .showSnackBar(SnackBar(
-                                  content: Text(
-                                      'Kliknięto podwójnie id: ${konto['id']}'),
-                                  duration: const Duration(seconds: 1),
-                                ));
-                                Navigator.of(context).push(MaterialPageRoute(
-                                    builder: (context) => KontoDetailsPage(
-                                          kontoDocId: konto['id'],
-                                        )));
-                              }
-                              selectedRowDocId = konto['id'];
-                            },
-                            cells: <DataCell>[
-                              DataCell(Text(konto['login'])),
-                              DataCell(Text(konto['kontrahent'])),
-                              DataCell(Text(konto['host'])),
-                              DataCell(Text(konto['typ konta'])),
-                              DataCell(Text(konto['uwagi'])),
-                            ])
-                    ]);
-                  }
-                  return const Text('Ładowanie');
-                })
-          ],
+                    builder: (context) => const KontoNewPage())),
+                icon: const Icon(Icons.person_add),
+                label: const Text('Dodaj konto'),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  const Icon(Icons.filter_alt),
+                  const Text('Host'),
+                  dropdownButtonFromCollection(hosty),
+                ],
+              ),
+              FutureBuilder(
+                  future: konta.get(),
+                  builder: (BuildContext context,
+                      AsyncSnapshot<QuerySnapshot> snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      List<dynamic> kontaList = snapshot.data!.docs.map((doc) {
+                        dynamic tempMap = doc.data();
+                        tempMap['id'] = doc.id;
+                        return tempMap;
+                      }).toList();
+                      return DataTable(
+                          showCheckboxColumn: false,
+                          columns: const <DataColumn>[
+                            DataColumn(
+                                label: Text('Login',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold))),
+                            DataColumn(
+                                label: Text('Kontrahent',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold))),
+                            DataColumn(
+                                label: Text('Host',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold))),
+                            DataColumn(
+                                label: Text('Typ konta',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold))),
+                            DataColumn(
+                                label: Text('Uwagi',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold))),
+                          ],
+                          rows: <DataRow>[
+                            for (var konto in kontaList)
+                              DataRow(
+                                  onSelectChanged: (value) {
+                                    if (selectedRowDocId == konto['id']) {
+                                      ScaffoldMessenger.of(context)
+                                          .showSnackBar(SnackBar(
+                                        content: Text(
+                                            'Kliknięto podwójnie id: ${konto['id']}'),
+                                        duration: const Duration(seconds: 1),
+                                      ));
+                                      Navigator.of(context).push(
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  KontoDetailsPage(
+                                                    kontoDocId: konto['id'],
+                                                  )));
+                                    }
+                                    selectedRowDocId = konto['id'];
+                                  },
+                                  cells: <DataCell>[
+                                    DataCell(Text(konto['login'])),
+                                    DataCell(Text(konto['kontrahent'])),
+                                    DataCell(Text(konto['host'])),
+                                    DataCell(Text(konto['typ konta'])),
+                                    DataCell(Text(konto['uwagi'])),
+                                  ])
+                          ]);
+                    }
+                    return const Text('Ładowanie');
+                  }),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  FutureBuilder dropdownButtonFromCollection(CollectionReference collection) {
+    // List<String> docs = [for (var doc in collection.docs) doc['nazwa']];
+    // List<DropdownMenuItem<String>> dropdownItems = [
+    //   for (var item in docs)
+    //     DropdownMenuItem(
+    //       value: item,
+    //       child: Text(item),
+    //     )
+    // ];
+    return FutureBuilder(
+      future: collection.get(),
+      builder: (BuildContext context, snapshot) {
+        return Text('eli');
+      },
+    );
+    // DropdownButton(items: dropdownItems, onChanged: null);
   }
 }
