e94fad463270bc94f87f861a1f725fc0bc497bba
wkedsxx <dawid.debest@gmail.com>
2023-04-07 16:27:40 +0200

    Przy dodawaniu nowego konta dodałem listy rozwijane

--- lib/views/konto_new_page.dart
+++ lib/views/konto_new_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import '/views/ui_elements/collection_dropdown.dart';
 
 class KontoNewPage extends StatefulWidget {
   const KontoNewPage({super.key});
@@ -10,14 +11,41 @@ class KontoNewPage extends StatefulWidget {
 
 class _KontoNewPageState extends State<KontoNewPage> {
   CollectionReference konta = FirebaseFirestore.instance.collection('konta');
+  CollectionReference kontrahenci =
+      FirebaseFirestore.instance.collection('kontrahenci');
+  CollectionReference hosty = FirebaseFirestore.instance.collection('hosty');
+  CollectionReference typyKont =
+      FirebaseFirestore.instance.collection('typy kont');
+
+  String? kontrahentValue, hostValue, typKontaValue;
+  void updateKontrahent(String? newValue) {
+    setState(() {
+      kontrahentValue = newValue;
+      print(kontrahentValue);
+    });
+  }
+
+  void updateHost(String? newValue) {
+    setState(() {
+      hostValue = newValue;
+      print(hostValue);
+    });
+  }
+
+  void updateTypkonta(String? newValue) {
+    setState(() {
+      typKontaValue = newValue;
+      print(typKontaValue);
+    });
+  }
 
   var loginController = TextEditingController();
   var hasloController = TextEditingController();
   var nazwaBDController = TextEditingController();
   var uwagiController = TextEditingController();
-  var kontrahentController = TextEditingController();
-  var hostController = TextEditingController();
-  var typKontaController = TextEditingController();
+  // var kontrahentController = TextEditingController();
+  // var hostController = TextEditingController();
+  // var typKontaController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,9 +58,15 @@ class _KontoNewPageState extends State<KontoNewPage> {
               tile(Icons.key, 'Hasło:', hasloController),
               tile(Icons.table_rows_rounded, 'Nazwa BD:', nazwaBDController),
               tile(Icons.feed, 'Uwagi:', uwagiController),
-              tile(Icons.groups, 'Kontrahent:', kontrahentController),
-              tile(Icons.lan, 'Host:', hostController),
-              tile(Icons.manage_accounts, 'Typ konta:', typKontaController),
+              tileDropdown(
+                Icons.groups,
+                'Kontrahent:',
+                kontrahenci,
+                updateKontrahent,
+              ),
+              tileDropdown(Icons.lan, 'Host:', hosty, updateHost),
+              tileDropdown(Icons.manage_accounts, 'Typ konta:', typyKont,
+                  updateTypkonta),
               const SizedBox(height: 100),
               ElevatedButton.icon(
                 onPressed: () {
@@ -41,9 +75,9 @@ class _KontoNewPageState extends State<KontoNewPage> {
                     'haslo': hasloController.text,
                     'nazwa BD': nazwaBDController.text,
                     'uwagi': uwagiController.text,
-                    'kontrahent': kontrahentController.text,
-                    'host': hostController.text,
-                    'typ konta': typKontaController.text,
+                    'kontrahent': kontrahentValue,
+                    'host': hostValue,
+                    'typ konta': typKontaValue,
                     'data utworzenia': FieldValue.serverTimestamp()
                   };
                   konta.add(newKonto);
@@ -63,6 +97,21 @@ class _KontoNewPageState extends State<KontoNewPage> {
         Text(title),
         const SizedBox(width: 50),
         Expanded(child: TextField(controller: controller))
+      ],
+    );
+  }
+
+  Row tileDropdown(IconData icon, String title,
+      CollectionReference collectionRef, Function callback) {
+    return Row(
+      children: [
+        Icon(icon),
+        Text(title),
+        const SizedBox(width: 50),
+        CollectionDropdown(
+          collectionRef: collectionRef,
+          callback: callback,
+        ),
       ],
     );
   }
